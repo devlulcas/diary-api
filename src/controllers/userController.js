73f0e4105f70f42aa60e553cf9572ed8c7e57802
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-09 18:16:13 -0300

    add: controller de usuario sem funcionalidade real

---
+++ src/controllers/userController.js
@@ -0,0 +1,15 @@
+class UserController {
+  registerUser(req, res) {
+    res.json({ status: "OK REGISTERED" });
+  }
+
+  loginUser(req, res) {
+    res.json({ status: "OK LOGGED IN" });
+  }
+
+  updateUser(req, res) {
+    res.json({ status: "OK LOGGED IN" });
+  }
+}
+
+module.exports = LoginController;
