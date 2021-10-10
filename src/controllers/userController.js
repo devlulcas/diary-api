a42fce31388e8b8bf9044fad3b603f028ed24c18
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-10 04:44:12 -0300

    add: verificacao de campos vazios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,23 @@
 class UserController {
   registerUser(req, res) {
+    const data = req.body;
+
+    const userData = [
+      data.userName,
+      data.userEmail,
+      data.userPassword,
+      data.userBirthDate,
+      data.userTermsAndConditions,
+    ];
+
+    // Não podemos seguir se algum campo estiver vazio
+    for (const field of userData) {
+      if (!field)
+        return res
+          .status(403)
+          .send({ error: "there is a parameter missing in you request" });
+    }
+
     res.json({ status: "OK REGISTERED" });
   }
 
