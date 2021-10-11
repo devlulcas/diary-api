bcf68a48c223d64806eb8f42579999f6fc54b72c
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-11 18:12:45 -0300

    add: validacao completa dos dados recebidos

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,32 +1,45 @@
+const ValidationService = require("../services/validationService");
+
 class UserController {
+  // Registro do usuário completo (com validação)
   registerUser(req, res) {
     const data = req.body;
+    const { userName, userEmail, userPassword, userTermsAndConditions } = data;
 
-    const userData = [
-      data.userName,
-      data.userEmail,
-      data.userPassword,
-      data.userBirthDate,
-      data.userTermsAndConditions,
-    ];
+    // Validando os dados recebidos
+    const vs = new ValidationService(res);
+    const { isDataValid, message } = vs.verifyUserData(data, "register");
+    if (!isDataValid) return vs.errorLog(message);
 
-    // Não podemos seguir se algum campo estiver vazio
-    for (const field of userData) {
-      if (!field)
-        return res
-          .status(403)
-          .send({ error: "there is a parameter missing in you request" });
-    }
-
+    // Caso os dados sejam válidos...
     res.json({ status: "OK REGISTERED" });
   }
 
+  // Login do usuário completo (com validação)
   loginUser(req, res) {
+    const data = req.body;
+    const { userEmail, userPassword } = data;
+
+    // Validando os dados recebidos
+    const vs = new ValidationService(res);
+    const { isDataValid, message } = vs.verifyUserData(data, "login");
+    if (!isDataValid) return vs.errorLog(message);
+
+    // Caso os dados sejam válidos...
     res.json({ status: "OK LOGGED IN" });
   }
 
   updateUser(req, res) {
-    res.json({ status: "OK LOGGED IN" });
+    const data = req.body;
+    const { userEmail, userPassword } = data;
+
+    // Validando os dados recebidos
+    const vs = new ValidationService(res);
+    const { isDataValid, message } = vs.verifyUserData(data, "login");
+    if (!isDataValid) return vs.errorLog(message);
+
+    // Caso os dados sejam válidos...
+    res.json({ status: "OK CHANGED" });
   }
 }
 
