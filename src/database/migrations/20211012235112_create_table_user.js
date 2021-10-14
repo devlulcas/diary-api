a7dde9e52d35e7d1810de0ef93c2f9c64a8d2157
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-14 14:56:01 -0300

    add: migrations para a criacao da tabela de usuario

---
+++ src/database/migrations/20211012235112_create_table_user.js
@@ -0,0 +1,18 @@
+exports.up = (knex) =>
+  knex.schema.createTable("user", (table) => {
+    /*
+    _________________________________________________
+    |                      user                     |
+    -------------------------------------------------
+    | id | username | email | password | created_at |
+    -------------------------------------------------
+    */
+    table.increments("id");
+    table.text("username").unique().notNullable();
+    table.text("email").unique().notNullable();
+    table.text("password").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+
+exports.down = (knex) => knex.schema.dropTable("user");
