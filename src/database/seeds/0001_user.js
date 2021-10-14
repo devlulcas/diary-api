b7ff853bc68bcc4126b4d73d4cb80567b81f2b29
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-14 14:57:15 -0300

    add: seeds para popular o banco de dados para testes

---
+++ src/database/seeds/0001_user.js
@@ -0,0 +1,22 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex("user")
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex("user").insert([
+        {
+          username: "test",
+          email: "test@test.com",
+          password:
+            "$2a$10$z7E3HRtz6Zc2iDib8y7jOexVdUORj8xSSe2Ny9xxFXZqgckK0.nDa",
+        },
+        {
+          username: "tset",
+          email: "tset@tset.com",
+          password:
+            "$2a$10$R24l9gn5ajAqpEEXY04AQevNaor8QH08WSGfYoNIiGjQUUHt/CLki",
+        },
+      ]);
+    });
+};
