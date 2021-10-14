5bb1561d547c65d5c54dc0aeaefb2a1ec8df8fdf
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-14 14:54:25 -0300

    change: agora realmente possui informacoes do usuario para trabalhar a autorizacao

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,61 +1,70 @@
 const ValidationService = require("../services/validationService");
 const UserService = require("../services/userService");
+
 class UserController {
-  constructor() {
-    this._userService = new UserService();
-  }
-
   // Registro do usuário completo (com validação)
-  registerUser(req, res) {
+  async registerUser(req, res) {
     const userData = req.body;
-
     // Validando os dados recebidos
     const validationService = new ValidationService(res);
     const { isDataValid, message } = validationService.verifyUserData(
       userData,
       "register"
     );
-
     if (!isDataValid) return validationService.errorLog(message);
-
-    // Caso os dados sejam válidos criamos o usuário ou retornamos um que já existe
-    const user = await this._userService.create(data);
-
-    res.json({ status: "OK REGISTERED", user });
+    // Caso os dados sejam válidos pedimos para criar o usuário
+    const userService = new UserService();
+    const user = await userService.create(userData);
+    const successMessage = "OK REGISTERED";
+    const errorMessage = "CAN NOT REGISTER USER";
+    if (user.error) {
+      return res.status(409).send({ status: errorMessage, user });
+    }
+    // Caso a operação seja bem sucedida prosseguimos
+    return res.send({ status: successMessage, user });
   }
 
   // Login do usuário completo (com validação)
-  loginUser(req, res) {
-    const data = req.body;
-
+  async loginUser(req, res) {
+    const userData = req.body;
     // Validando os dados recebidos
     const validationService = new ValidationService(res);
     const { isDataValid, message } = validationService.verifyUserData(
-      data,
+      userData,
       "login"
     );
     if (!isDataValid) return validationService.errorLog(message);
-
-    // Caso os dados sejam válidos...
-    const user = await this._userService.get(data);
-
-    res.json({ status: "OK LOGGED IN", user });
+    // Caso os dados sejam válidos pedimos para buscá-lo
+    const userService = new UserService();
+    const user = await userService.get(userData);
+    const successMessage = "OK LOGGED IN";
+    const errorMessage = "CAN NOT LOGIN THIS USER";
+    if (user.error) {
+      return res.status(409).send({ status: errorMessage, user });
+    }
+    // Caso a operação seja bem sucedida prosseguimos
+    return res.send({ status: successMessage, user });
   }
 
-  updateUser(req, res) {
-    const data = req.body;
-
+  async updateUser(req, res) {
+    const userData = req.body;
     // Validando os dados recebidos
     const validationService = new ValidationService(res);
     const { isDataValid, message } = validationService.verifyUserData(
-      data,
+      userData,
       "login"
     );
     if (!isDataValid) return validationService.errorLog(message);
-
     // Caso os dados sejam válidos...
-    const user = await this._userService.update(data);
-    res.json({ status: "OK CHANGED", user });
+    const userService = new UserService();
+    const user = await userService.update(userData);
+    const successMessage = "OK CHANGED";
+    const errorMessage = "CAN NOT CHANGE INFO";
+    if (user.error) {
+      return res.status(403).send({ status: errorMessage, user });
+    }
+    // Caso a operação seja bem sucedida prosseguimos
+    return res.send({ status: successMessage, user });
   }
 }
 
