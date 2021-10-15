f574cee0aaceb320445fece3c73eac6dbd3f7c8e
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-15 16:09:07 -0300

    modificado: adicionando try catch para o tratamento de erros

--- src/services/userService.js
+++ src/services/userService.js
@@ -10,38 +10,54 @@ class UserService {
 
   // Responsável por passar os dados prontos para serem inseridos no banco de dados
   async create(userData) {
-    // Data
-    const { userName, userEmail, userPassword } = userData;
-    const treatedData = this._getTreatedData(userName, userEmail);
-    const treatedPassword = await this._hashPassword(userPassword);
-    // Retorna usuário recém criado ou erro
-    const newUser = await this._databaseService.createNewUser(
-      ...treatedData,
-      treatedPassword
-    );
-    return newUser;
+    try {
+      // Data
+      const { userName, userEmail, userPassword } = userData;
+      const treatedData = this._getTreatedData(userName, userEmail);
+      const treatedPassword = await this._hashPassword(userPassword);
+      // Retorna usuário recém criado ou erro
+      const newUser = await this._databaseService.createNewUser(
+        ...treatedData,
+        treatedPassword
+      );
+      return newUser;
+    } catch (error) {
+      throw error;
+    }
   }
 
   // Responsável por pegar os dados do banco de dados a partir de dados tratados
   async get(userData) {
-    // Data
-    const { userName, userEmail } = userData;
-    const treatedData = this._getTreatedData(userName, userEmail);
-    // Retorna usuário já existente
-    const existingUser = await this._databaseService.findUser(...treatedData);
-    return existingUser;
+    try {
+      // Data
+      const { userName, userEmail } = userData;
+      const treatedData = this._getTreatedData(userName, userEmail);
+      // Retorna usuário já existente
+      const existingUser = await this._databaseService.findUser(...treatedData);
+      return existingUser;
+    } catch (error) {
+      throw error;
+    }
   }
 
   // Responsável por tornar o armazenamento de senhas mais seguro
   async _hashPassword(userPassword) {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(userPassword, salt);
-    return hashedPassword;
+    try {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(userPassword, salt);
+      return hashedPassword;
+    } catch (error) {
+      throw error;
+    }
   }
 
   // Verifica se a senha recebida é compatível com a armazenada no banco de dados
   async _isCorrectPassword(userPassword, storedHashedPassword) {
-    return await bcrypt.compare(userPassword, storedHashedPassword);
+    try {
+      return await bcrypt.compare(userPassword, storedHashedPassword);
+    } catch (error) {
+      throw error;
+    }
   }
 
   // Retorna os dados tratados
