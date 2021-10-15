36e4f87d3d8036bf9ebe4f1afb6c9760a2b39ecb
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-15 17:11:26 -0300

    modificado:retorna erros reais e nao objetos com um pseudo erro como propriedade

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -2,29 +2,32 @@ const knex = require("../database");
 class DatabaseService {
   // OPERAÇÕES RELACIONADAS AOS USUÁRIOS
   async createNewUser(username, email, password) {
-    // Caso algum dos dados bata com um existente devemos parar por aqui
-    const userId = await knex("user")
-      .select("id")
-      .where({ email })
-      .orWhere({ username });
-
-    if (userId.length) {
-      const errorMessage = "Someone already took this email or username";
-      return { errorMessage, error: true };
+    try {
+      // Caso algum dos dados bata com um existente devemos parar por aqui
+      const userId = await knex("user")
+        .select("id")
+        .where({ email })
+        .orWhere({ username });
+      if (userId.length) {
+        throw new Error("Someone already took this email or username");
+      }
+      // Caso nem o email e nem o username tenham sido pegos, criamos o usuário
+      await knex("user").insert({ username, email, password });
+      const user = await this.findUser(username, email);
+      return user;
+    } catch (error) {
+      throw error;
     }
-    // Caso nem o email e nem o username tenham sido pegos, criamos o usuário
-    await knex("user").insert({ username, email, password });
-    const user = await this.findUser(username, email);
-    return { user, error: false };
   }
 
   async findUser(username, email) {
-    const user = await knex("user").where({ username, email });
-    if (!user[0]) {
-      const errorMessage = "This user does not exist";
-      return { errorMessage, error: true };
+    try {
+      const user = await knex("user").where({ username, email })[0];
+      if (!user) throw new Error("This user does not exist");
+      return user;
+    } catch (error) {
+      throw error;
     }
-    return { user, error: false };
   }
 }
 
