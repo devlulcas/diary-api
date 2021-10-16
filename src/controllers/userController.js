696d5ba11320e5820ecff9768f79f452f238a895
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-15 22:28:00 -0300

    modificado: rework para melhorar os tratamentos de erro

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,70 +1,64 @@
 const ValidationService = require("../services/validationService");
 const UserService = require("../services/userService");
+const DiaryService = require("../services/diaryService");
 
 class UserController {
   // Registro do usuário completo (com validação)
-  async registerUser(req, res) {
-    const userData = req.body;
-    // Validando os dados recebidos
-    const validationService = new ValidationService(res);
-    const { isDataValid, message } = validationService.verifyUserData(
-      userData,
-      "register"
-    );
-    if (!isDataValid) return validationService.errorLog(message);
-    // Caso os dados sejam válidos pedimos para criar o usuário
-    const userService = new UserService();
-    const user = await userService.create(userData);
-    const successMessage = "OK REGISTERED";
-    const errorMessage = "CAN NOT REGISTER USER";
-    if (user.error) {
-      return res.status(409).send({ status: errorMessage, user });
+  async registerUser(req, res, next) {
+    try {
+      const userData = req.body;
+      // Validando os dados recebidos
+      const validationService = new ValidationService(res);
+      validationService.verifyUserData(userData, "register");
+      // Caso os dados sejam válidos pedimos para criar o usuário
+      const userService = new UserService();
+      const user = await userService.create(userData);
+      // Caso a operação seja bem sucedida prosseguimos criando o diário
+      const diaryService = new DiaryService(user.id);
+      const diary = await diaryService.create();
+      // Tudo correu como o planejado
+      const successMessage = "OK REGISTERED";
+      return res.status(201).send({ status: successMessage, user, diary });
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
-    // Caso a operação seja bem sucedida prosseguimos
-    return res.send({ status: successMessage, user });
   }
 
   // Login do usuário completo (com validação)
-  async loginUser(req, res) {
-    const userData = req.body;
-    // Validando os dados recebidos
-    const validationService = new ValidationService(res);
-    const { isDataValid, message } = validationService.verifyUserData(
-      userData,
-      "login"
-    );
-    if (!isDataValid) return validationService.errorLog(message);
-    // Caso os dados sejam válidos pedimos para buscá-lo
-    const userService = new UserService();
-    const user = await userService.get(userData);
-    const successMessage = "OK LOGGED IN";
-    const errorMessage = "CAN NOT LOGIN THIS USER";
-    if (user.error) {
-      return res.status(409).send({ status: errorMessage, user });
+  async loginUser(req, res, next) {
+    try {
+      const userData = req.body;
+      // Validando os dados recebidos
+      const validationService = new ValidationService(res);
+      validationService.verifyUserData(userData, "login");
+      // Caso os dados sejam válidos pedimos para buscá-lo
+      const userService = new UserService();
+      const user = await userService.get(userData);
+      // Tudo correu como o planejado
+      const successMessage = "OK LOGGED IN";
+      return res.status(201).send({ status: successMessage, user });
+    } catch (error) {
+      next(error);
     }
-    // Caso a operação seja bem sucedida prosseguimos
-    return res.send({ status: successMessage, user });
   }
 
-  async updateUser(req, res) {
-    const userData = req.body;
-    // Validando os dados recebidos
-    const validationService = new ValidationService(res);
-    const { isDataValid, message } = validationService.verifyUserData(
-      userData,
-      "login"
-    );
-    if (!isDataValid) return validationService.errorLog(message);
-    // Caso os dados sejam válidos...
-    const userService = new UserService();
-    const user = await userService.update(userData);
-    const successMessage = "OK CHANGED";
-    const errorMessage = "CAN NOT CHANGE INFO";
-    if (user.error) {
-      return res.status(403).send({ status: errorMessage, user });
+  // Atualização de usuário completo (com validação)
+  async updateUser(req, res, next) {
+    try {
+      const userData = req.body;
+      // Validando os dados recebidos
+      const validationService = new ValidationService(res);
+      validationService.verifyUserData(userData, "update");
+      // Caso os dados sejam válidos...
+      const userService = new UserService();
+      const user = await userService.update(userData);
+      // Tudo correu como o planejado
+      const successMessage = "OK CHANGED";
+      return res.send({ status: successMessage, user });
+    } catch (error) {
+      next(error);
     }
-    // Caso a operação seja bem sucedida prosseguimos
-    return res.send({ status: successMessage, user });
   }
 }
 
