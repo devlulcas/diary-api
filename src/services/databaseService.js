1407192a39b777cabc2651449bd15e01334edca8
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-16 12:32:58 -0300

    modificado: nao mais cria a variavel e depois retorna, agora a operacao de retorno e feita diretamente

--- src/services/databaseService.js
+++ src/services/databaseService.js
@@ -13,8 +13,7 @@ class DatabaseService {
       }
       // Caso nem o email e nem o username tenham sido pegos, criamos o usuário
       await knex("user").insert({ username, email, password });
-      const user = await this.findUser(username, email);
-      return user;
+      return await this.findUser(username, email);
     } catch (error) {
       throw error;
     }
@@ -22,9 +21,9 @@ class DatabaseService {
 
   async findUser(username, email) {
     try {
-      const user = await knex("user").where({ username, email })[0];
-      if (!user) throw new Error("This user does not exist");
-      return user;
+      const user = await knex("user").where({ username, email });
+      if (!user[0]) throw new Error("This user does not exist");
+      return user[0];
     } catch (error) {
       throw error;
     }
@@ -37,8 +36,7 @@ class DatabaseService {
       await knex("user")
         .where({ id: userId })
         .update({ username, email, password });
-      const user = await this.findUser(username, email);
-      return user;
+      return await this.findUser(username, email);
     } catch (error) {
       console.log(error);
       throw error;
@@ -48,10 +46,8 @@ class DatabaseService {
   // OPERAÇÕES RELACIONADAS AOS DIÁRIOS
   async createNewDiary(userId, path) {
     try {
-      if (!userId) throw new Error("Could not create diary, empty user id");
       await knex("diary").insert({ user_id: userId, path });
-      const diary = await this.findDiary(userId);
-      return diary;
+      return await this.findDiaryPath(userId);
     } catch (error) {
       throw error;
     }
@@ -59,9 +55,7 @@ class DatabaseService {
 
   async findDiaryPath(userId) {
     try {
-      if (!userId) throw new Error("Could not find diary, empty user id");
-      const diaryPath = await knex("diary").where({ user_id: userId })[0];
-      return diaryPath;
+      return await knex("diary").where({ user_id: userId })[0];
     } catch (error) {
       throw error;
     }
@@ -71,9 +65,8 @@ class DatabaseService {
   async createNewConfig(userId, userConfig) {
     try {
       // Se os dados passados não forem os desejados devemos retornar erros
-      if (!userId) throw new Error("Could not create config, empty user id");
       const { colorScheme } = userConfig;
-      if (typeof colorScheme != "number") {
+      if (Number.isInteger(colorScheme)) {
         throw new Error("Color scheme has to be a number");
       }
       // Caso contrário podemos cadastrar as configurações
@@ -81,8 +74,7 @@ class DatabaseService {
         user_id: userId,
         color_scheme: colorScheme,
       });
-      const config = await this.findConfig(userId);
-      return config;
+      return await this.findConfig(userId);
     } catch (error) {
       throw error;
     }
@@ -90,11 +82,8 @@ class DatabaseService {
 
   async findConfig(userId) {
     try {
-      // Se os dados passados não forem os desejados devemos retornar erros
-      if (!userId) throw new Error("Could not create config, empty user id");
-      // Caso contrário seguimos com a consulta
-      const config = await knex("config").where({ user_id: userId })[0];
-      return config;
+      const config = await knex("config").where({ user_id: userId });
+      return config[0];
     } catch (error) {
       throw error;
     }
@@ -103,17 +92,15 @@ class DatabaseService {
   async updateConfig(userId, newUserConfig) {
     try {
       // Se os dados passados não forem os desejados devemos retornar erros
-      if (!userId) throw new Error("Could not create config, empty user id");
       const { colorScheme } = newUserConfig;
-      if (typeof colorScheme != "number") {
+      if (Number.isInteger(colorScheme)) {
         throw new Error("Color scheme has to be a number");
       }
       // Caso contrário seguimos com o update
       await knex("config")
         .where({ user_id: userId })
         .update({ color_scheme: colorScheme });
-      const updatedConfig = await this.findConfig(userId);
-      return updatedConfig;
+      return await this.findConfig(userId);
     } catch (error) {
       throw error;
     }
