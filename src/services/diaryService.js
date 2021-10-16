106d2580c46c20a20f759d3bf998fb2ec0c63455
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-15 22:50:30 -0300

    adicionado: servicos basicos de manipulacao dos diarios

---
+++ src/services/diaryService.js
@@ -0,0 +1,74 @@
+const DatabaseService = require("../services/databaseService");
+const path = require("path");
+const fs = require("fs");
+
+class DiaryService {
+  // Criar diário pela primeira vez
+  constructor(userId) {
+    this._databaseService = new DatabaseService();
+    this.userId = userId;
+    this.diaryPath = this._diaryPath();
+  }
+
+  _diaryPath() {
+    const diaryFilename = `${this.userId}_diary.txt`;
+    const fullPath = path.join(
+      __dirname,
+      "..",
+      "..",
+      "/content",
+      "/diaries",
+      diaryFilename
+    );
+    return fullPath;
+  }
+
+  // Utilizado para criar o arquivo de diário apenas quando um novo usuário surge
+  async create() {
+    try {
+      fs.writeFile(this.diaryPath, "Querido diário...", (error) => {
+        if (error) {
+          console.log(err);
+          throw new Error("Could not proceed file operation");
+        }
+      });
+      return await this._databaseService.createNewDiary(
+        this.userId,
+        this.diaryPath
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Responsável por pegar os dados do banco de dados a partir de dados tratados
+  async get() {
+    try {
+      const readStream = fs.createReadStream(this.diaryPath, {
+        encoding: "utf-8",
+      });
+      // Retorna pedaços de dados para serem entregues ao usuário
+      readStream.on("data", (chunk) => {
+        return chunk;
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // Responsável por alterar as informações atuais de um usuário
+  async update(textContent) {
+    try {
+      fs.writeFile(this.diaryPath, textContent, (error) => {
+        if (error) {
+          console.log(err);
+          throw new Error("Could not proceed file operation");
+        }
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = DiaryService;
