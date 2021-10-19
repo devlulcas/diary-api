67fbd895123896c14de31bbd03b8e4223ee4a0e8
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-19 10:22:30 -0300

    adicionado: funcionalidade real com a adicao do configService

--- src/controllers/configController.js
+++ src/controllers/configController.js
@@ -1,14 +1,28 @@
+const ConfigService = require("../services/configService");
+
 class ConfigController {
-  getConfig(req, res) {
-    res.json({ status: "OK GET CONFIG" });
+  async getConfig(req, res, next) {
+    try {
+      const userId = req.userId;
+      const configService = new ConfigService(userId);
+      const config = await configService.get();
+      return res.json({ success: true, status: "GOT CONFIG", config });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  createConfig(req, res) {
-    res.json({ status: "OK CREATE CONFIG" });
-  }
-
-  updateConfig(req, res) {
-    res.json({ status: "OK UPDATE CONFIG" });
+  async updateConfig(req, res, next) {
+    try {
+      const userId = req.userId;
+      const config = req.body;
+      console.log(config);
+      const configService = new ConfigService(userId);
+      const success = await configService.update(config);
+      return res.json({ success, status: "UPDATED CONFIG" });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
