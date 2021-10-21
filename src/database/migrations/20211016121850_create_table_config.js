5f03bc0df82e32bdb68a6291df92a4a738a2a184
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-20 21:44:27 -0300

    correcao: nao e necessario a relacao um para muitos na configuracao

--- src/database/migrations/20211016121850_create_table_config.js
+++ src/database/migrations/20211016121850_create_table_config.js
@@ -1,22 +1,14 @@
 exports.up = (knex) =>
   knex.schema.createTable("config", (table) => {
     /*
-    _________________________________________________________
-    |                         config                        |
-    ---------------------------------------------------------
-    | id | color_scheme | user_id | created_at | updated_at |
-    ---------------------------------------------------------
+    _______________________________________________
+    |                 config                      |
+    -----------------------------------------------
+    | id | color_scheme | created_at | updated_at |
+    -----------------------------------------------
     */
     table.increments("id");
-    table.integer("color_scheme").unique().notNullable().defaultTo(0);
-    // Relacionamento com user
-    table
-      .integer("user_id")
-      .notNullable()
-      .references("id")
-      .inTable("user")
-      .onDelete("CASCADE")
-      .index();
+    table.integer("color_scheme").notNullable().defaultTo(0);
     // Timestamps
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
