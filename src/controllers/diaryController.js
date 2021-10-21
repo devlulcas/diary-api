0473d4ec79a986cd5a2cfbc4ab05b85e4d4f91cb
devlulcas <lucasalvesrego.contato@gmail.com>
2021-10-20 21:59:25 -0300

    modificado: agora utiliza a funcao geradora da string de path para fazer as consultas

--- src/controllers/diaryController.js
+++ src/controllers/diaryController.js
@@ -1,9 +1,12 @@
 const DiaryService = require("../services/diaryService");
+const { diaryPath } = require("../utils");
+
 class DiaryController {
   getDiaryContent(req, res, next) {
     try {
       const userId = req.userId;
-      const diaryService = new DiaryService(userId);
+      const diaryRelativePath = diaryPath(userId);
+      const diaryService = new DiaryService(diaryRelativePath);
       const diaryReadStream = diaryService.get();
       diaryReadStream.pipe(res);
     } catch (error) {
@@ -14,7 +17,8 @@ class DiaryController {
   updateDiaryContent(req, res, next) {
     try {
       const userId = req.userId;
-      const diaryService = new DiaryService(userId);
+      const diaryRelativePath = diaryPath(userId);
+      const diaryService = new DiaryService(diaryRelativePath);
       req.pipe(diaryService.update());
       res.json({ success: true, status: "OK GET DIARY" });
     } catch (error) {
